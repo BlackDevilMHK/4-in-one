d1b2800dfe9ee6fde3bf81677d1645559285e3b9
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-02 18:02:15 +0600

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#installation
+
+clear
+
+echo ""
+echo ""
+echo "" SUBSCRIBE MY CHANNEL ""
+xdg-open https://youtube.com/channel/UCfRYPIUkuagLYibzF0o6Qgw
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+echo "Loading..."
+sleep 1
+clear
+
+toilet -f future "BlackDevilMHK"
+echo "-------------------------------------------------------"
+echo   "Created  : BlackDevilMHK $white"
+echo   "Contact  : facebook.com/BlackDevilMHK $white"
+echo   "Telegram  : t.me/BlackDevilMHK $white"
+echo "-------------------------------------------------------"
+
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit  ... "
+echo ""
+sleep 1
+echo ""
+echo "BlackDevilMHK"
+sleep 1
+exit
+}
+
+apt update
+apt upgrade -y
+pkg install ruby -y 
+gem install toilet -y 
+pkg install lolcat
+pkg install figlet
+apt install git php openssh curl -y
+pkg install python2 -y 
+pkg install nano php -y
+pip install mechanize
+pip install --upgrade pip
+
+echo ""
+echo "CODE_BY_BlackDevilMHK"
+echo "________________________"
