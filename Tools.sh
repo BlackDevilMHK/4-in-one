7e47dc0c1bcd816a23c3e5b7924c9d659dcca254
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-02 17:43:41 +0600

    Create Tools.sh

---
+++ Tools.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# version 2.1
+
+clear
+
+
+echo ""
+echo -e "SUBSCRIBE MY CHANNEL"
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+echo "Loading..." |
+sleep 2
+clear
+
+toilet -f future "BlackDevilMHK"
+echo "-------------------------------------------------------"
+echo   "Author   : B2A6K D3V1L-MHK $white"
+echo   "Contact  : https://facebook.com/BlackDevilMHK $white"
+echo   "YouTube  : Black Devil MHK $white
+echo "-------------------------------------------------------"
+echo "                           v2.0              " |
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit 🚪 ... "
+echo ""
+echo  "⚠️NOT FOR ILLEGEL USE⚠️"
+sleep 1
+echo ""
+echo "BlackDevilMHK" 
+echo ""
+echo "BYE BYE 👋👋..." 
+echo ""
+echo "COME BACK 🔜" 
+sleep 1
+exit
+}
