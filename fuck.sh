e753bb244f70b84dff9e1795c64bfb4cf80b03ed
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-03 15:13:54 +0600

    Update fuck.sh

--- fuck.sh
+++ fuck.sh
@@ -47,3 +47,7 @@ lagi=01
 while [ $lagi -lt 04 ];
 do
 echo ""
+echo -e "\e[1;101m\e[1;97m01\e[1;101m\e[0m\e[1;96m TOOL-X\e[0m\n";
+echo -e "\e[1;101m\e[1;97m02\e[1;101m\e[0m\e[1;96m LUCIFER\e[0m\n";
+echo -e "\e[1;101m\e[1;97m02\e[1;101m\e[0m\e[1;96m GMAILBRUTERV2\e[0m\n";
+echo -e "\e[1;101m\e[1;97m03\e[1;101m\e[0m\e[1;96m WEEMAN\e[0m\n";
