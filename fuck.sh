d848a8f20e2a3caeb302aeb8238afbbeec6a6727
B2A6K D3V1L-MHK <86191779+BlackDevilMHK@users.noreply.github.com>
2021-07-03 15:13:16 +0600

    Create fuck.sh

---
+++ fuck.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# version 2.1
+
+clear
+
+
+echo ""
+echo -e "SUBSCRIBE MY CHANNEL"
+echo ""
+
+echo -e "\e[101m Press Enter \e[0m"
+read a1
+
+echo "Loading..." |
+sleep 2
+clear
+
+toilet -f future "BlackDevilMHK"
+echo "-------------------------------------------------------"
+echo   "Author   : BlackDevilMHK $white"
+echo   "Contact  : https://facebook.com/BlackDevilMHK $white"
+echo   "YouTube  : Black Devil MHM $white"
+echo   "Telegram : t.me/BlackDevilMHK $white"
+echo "-------------------------------------------------------"
+echo "                           v2.0              " |
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  "Detected, Trying To Exit 🚪 ... "
+echo ""
+echo  "⚠️NOT FOR ILLEGEL USE⚠️"
+sleep 1
+echo ""
+echo "BlackDevilMHK" 
+echo ""
+echo "BYE BYE 👋👋..." 
+echo ""
+echo "COME BACK 🔜" 
+sleep 1
+exit
+}
+
+lagi=01
+while [ $lagi -lt 04 ];
+do
+echo ""
